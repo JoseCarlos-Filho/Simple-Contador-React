57178c88d4af82cda0839ad7702e62ddc6be6306
José Carlos <etijcvivi@gmail.com>
2022-04-04 22:11:16 -0300

    Contador simples usando React

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import { h1 } from 'react/lib/ReactDOMFactories';
+import App from './components/App';
+
+ReactDOM.render(
+    <App /> ,
+    document.getElementById("app")
+);
---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import { div } from 'react/lib/ReactDOMFactories';
+import Contador from './Contador'
+
+export default class App extends React.Component {
+    render() {
+        return <Contador />;
+    }
+}
